3c8ac4b763b95b30b306a62031859a2aa0b7ee41
Murad Ismayilov <murad.ismaylov.97@gmail.com>
2021-06-01 11:57:17 +0400

    Update MyView.java

--- RandomCircles/app/src/main/java/com/martiandeveloper/randomcircles/MyView.java
+++ RandomCircles/app/src/main/java/com/martiandeveloper/randomcircles/MyView.java
@@ -16,30 +16,40 @@ import java.util.Random;
 
 public final class MyView extends View {
 
+    // bu ekrana cekilendi, qelem kimi dusunun, rengini filan deyismek olur
     private Paint paint;
 
+    // Cekilenler ekrandan qiraga cixmasin deye, maximum ve minimum ekran olcusunu cunku random x ve y hisseleri yaradacagiq.
     private int maxX;
     private int maxY;
 
+    // Rengler listi
     private List<Integer> colorList;
 
+    // Nece eded daire cekileceyi
     private int circleCount;
 
+    // Constructor yeni class yaradildiqda cagirilan funksiya
     public MyView(@Nullable Context context) {
         super(context);
     }
 
+    // Constructor, parametrler ile, maximum ve minimumu, nece eded daire olacagini MainActivity' den aliriq
     public MyView(@Nullable Context context, int maxX, int maxY, int circleCount) {
         super(context);
 
+        // Painti yaradiriq, ve ici dolu olmasi ucun Style.FILL qururuq.
         paint = new Paint();
         paint.setStyle(Style.FILL);
 
+        // 32 burda dairenin diametridi, ekrandan cixiriq ki, dairenin yarisi qirada qalir bezen.
         this.maxX = maxX - 32;
         this.maxY = maxY - 32;
 
+        // List ucun funksiya
         setColorList();
 
+        // MainActivity'den alinan daire sayi
         this.circleCount = circleCount;
     }
 
@@ -57,17 +67,22 @@ public final class MyView extends View {
         colorList.add(Color.YELLOW);
     }
 
+    // onDraw burda View'in oz funksiyasidi biz View'i implement etmisik deye bu funksiyani override edirik
     @SuppressLint("DrawAllocation")
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        // For dongusu girilen input qeder daire ceke bilmek ucun
         for (int i = 0; i < circleCount; i++) {
+            // Random bir reng qururuq rengler listinden
             paint.setColor(colorList.get(new Random().nextInt(10)));
+            // Ve nehayet ekrana cekirik random hisselerde
             canvas.drawCircle(generateRandomXAndY()[0], generateRandomXAndY()[1], 32, paint);
         }
 
     }
 
+    // Random x ve y tapir ve istifade etmek ucun return edir.
     private int[] generateRandomXAndY() {
         int x, y;
         Random rn = new Random();
